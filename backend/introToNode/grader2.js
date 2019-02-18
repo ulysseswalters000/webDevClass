b13d48a5ccc7c3a7ffa56971afef0df84093aa67
Andrew Walters <ulysseswalters000@gmail.com>
2019-02-17 19:19:33 -0500

    another small refactor so it actually prints output

--- backend/introToNode/grader2.js
+++ backend/introToNode/grader2.js
@@ -6,7 +6,7 @@ function average(array){
   return Math.round( sum / array.length );
 }
 const scores = [ 98, 90, 89, 100, 100, 86, 94];
-average(scores); // should evaluate to 94
+console.log(average(scores)); // should evaluate to 94
 
 var scores2 = [ 40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
-average(scores2); // should evaluate to 68
+console.log(average(scores2)); // should evaluate to 68
