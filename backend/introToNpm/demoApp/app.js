d756c04fc5d3c0d8a67e4997cbd457683e734d39
Andrew Walters <ulysseswalters000@gmail.com>
2019-02-21 12:48:44 -0500

    added knock knock jokes package to app

--- backend/introToNpm/demoApp/app.js
+++ backend/introToNpm/demoApp/app.js
@@ -1,8 +1,11 @@
 // install cat-me package with npm i
 // this will install the needed package
 
-// include the cat-me package for use
+// include the cat-me & knock knock packages for use
 const catMe = require('cat-me');
+const joke  = require('knock-knock-jokes');
 
 // log a random cat to console
 console.log(catMe());
+// log random joke to console
+console.log(joke());
