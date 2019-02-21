fe4c0eb91811a64432e0d39f2a61f452b60b073c
Andrew Walters <ulysseswalters000@gmail.com>
2019-02-21 12:43:55 -0500

    better folder name

--- backend/introToNpm/catMeApp/app.js
+++ backend/introToNpm/catMeApp/app.js
@@ -1,2 +1,8 @@
+// install cat-me package with npm i
+// this will install the needed package
+
+// include the cat-me package for use
 const catMe = require('cat-me');
-console.log(catMe('nyan'));
+
+// log a random cat to console
+console.log(catMe());
--- backend/introToNpm/demoApp/app.js
+++
@@ -1,8 +0,0 @@
-// install cat-me package with npm i
-// this will install the needed package
-
-// include the cat-me package for use
-const catMe = require('cat-me');
-
-// log a random cat to console
-console.log(catMe());
