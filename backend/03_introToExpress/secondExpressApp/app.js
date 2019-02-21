b126b99a88ca6e78a317e16b903f6dd89226ebb1
Andrew Walters <ulysseswalters000@gmail.com>
2019-02-21 16:47:50 -0500

    more practice with express and npm, adding small complexity with route params

---
+++ backend/03_introToExpress/secondExpressApp/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('hi there, welcome to my assigment');
+});
+
+app.get('/speak/:animal', (req, res) => {
+  // stores parameter in mutable variable
+  let animal = req.params.animal;
+  // sends the return value of speak when passed param val
+  res.send(speak(animal));
+
+  // defines speak function, taking in animal as argv
+  function speak(animal) {
+    // defines key as the value of the passed arg
+    let key = animal;
+    // defines the obj of 'known' sounds
+    let animals = {
+      pig: 'Oink',
+      cow: 'Moo',
+      dog: 'Woof Woof!',
+      cat: 'Meow'
+    }
+
+    // if else that checks if animals has the key property
+    if (animals.hasOwnProperty(key)){
+      // if animals obj has the key
+      // return a string saying the param name + its value pair
+      return `The ${animal} says ${animals[key]}`;
+    } else {
+      // if key doesnt exist in the obj
+      // return the param value in a string
+      return `The ${animal} doesn't make a sound we know of...`;
+    }
+  }
+});
+
+app.get('/repeat/:word/:nTimes', (req, res) => {
+  let repeatedWord = req.params.word;
+  let numTimes = parseInt(req.params.nTimes);
+  res.send(repeater(repeatedWord, numTimes));
+  function repeater(word, times) {
+    let str = ``;
+    for(let i = 0; i < times; i++) {
+      str += `${word} `;
+    }
+     return str;
+  }
+});
+
+app.get('*', (req, res) => {
+  res.send('Sorry, page not found');
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('app listening on port 3000');
+});
