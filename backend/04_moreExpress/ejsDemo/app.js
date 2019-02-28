0c99abc6b36f468f1c113d117c45739d5e9075c0
Andrew Walters <ulysseswalters000@gmail.com>
2019-02-28 15:28:12 -0500

    continuing ejs study

--- backend/04_moreExpress/ejsDemo/app.js
+++ backend/04_moreExpress/ejsDemo/app.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const app = express();
 
+// View engine setup
+  // use statements
+  app.use(express.static('public'));
+
+  // set statements
+  app.set('view engine', 'ejs');
+
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.render('home.ejs');
+  res.render('home');
 });
 
 app.get('/fallinlovewith/:thing', (req, res) => {
   let thing = req.params.thing;
-  res.render('love.ejs', {thing: thing});
+  res.render('love', {thing: thing});
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('server listening on port 3000');
+app.get('/posts', (req, res) => {
+  let posts = [
+    {
+      title: 'Post 1',
+      author: 'Suzy'
+    },
+    {
+      title: 'Post 2',
+      author: 'Andrew'
+    },
+    {
+      title: 'Post 3',
+      author: 'Jessica'
+    }
+  ]
+  res.render('posts', {posts: posts});
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('server listening on port 8080');
 });
